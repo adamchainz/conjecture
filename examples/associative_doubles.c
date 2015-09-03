c273d5f8ffcc3c76d41f957304debef76b3ad5c9
David R. MacIver <david@drmaciver.com>
2015-09-03 16:45:18 +0100

    Oops names

--- examples/associative_doubles.c
+++ examples/associative_doubles.c
@@ -12,8 +12,7 @@ is associative. Because it's not, this should fail.
 
 */
 
-void test_small_sum(conjecture_context *context, void *data){
-  assert(data == NULL);
+void test_associative_doubles(conjecture_context *context, void *data){
   double x = conjecture_draw_double(context);
   double y = conjecture_draw_double(context);
   double z = conjecture_draw_double(context);
@@ -27,6 +26,6 @@ void test_small_sum(conjecture_context *context, void *data){
 int main(){
   conjecture_runner runner;
   conjecture_runner_init(&runner);
-  conjecture_run_test(&runner, test_small_sum, NULL);  
+  conjecture_run_test(&runner, test_associative_doubles, NULL);  
   conjecture_runner_release(&runner);
 }
